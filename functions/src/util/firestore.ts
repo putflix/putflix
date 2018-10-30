dd358e696cc85bb8ced2a3f7f18652426b8838d6
Marcus Weiner <marcus.weiner@gmail.com>
2018-10-30 21:29:23 +0100

    :art: Provide pseudo-orm for firestore collections

--- functions/src/util/firestore.ts
+++ functions/src/util/firestore.ts
@@ -3,3 +3,47 @@ import * as firebase from 'firebase-admin';
 export const firestore = new firebase.firestore.Firestore({
     timestampsInSnapshots: true
 });
+
+const dbCollections = {
+    tmdbMovies: firestore.collection('tmdb_movies'),
+    tmdbSeries: firestore.collection('tmdb_series'),
+    tmdbSeasons: firestore.collection('tmdb_seasons'),
+    tmdbEpisodes: firestore.collection('tmdb_episodes'),
+    tmdbQueue: firestore.collection('tmdb_queue'),
+    dedupMap: firestore.collection('dedup_map'),
+    accounts: firestore.collection('accounts'),
+};
+
+const dbSingles = {
+    movie: (id: string) => dbCollections.tmdbMovies.doc(id),
+    series: (id: string) => dbCollections.tmdbSeries.doc(id),
+    season: (id: string) => dbCollections.tmdbSeasons.doc(id),
+    episode: (id: string) => dbCollections.tmdbEpisodes.doc(id),
+    dedupMapping: (id: string) => dbCollections.dedupMap.doc(id),
+    account: (putIoAccountId: string) => dbCollections.accounts.doc(putIoAccountId),
+    user: (putIoAccountId: string) => {
+        const account = dbCollections.accounts.doc(putIoAccountId);
+        const collections = {
+            moviesCollection: account.collection('movies'),
+            seriesCollection: account.collection('series'),
+            seasonsCollection: account.collection('seasons'),
+            episodesCollection: account.collection('episodes'),
+            uncategorizedFiles: account.collection('uncategorized_files'),
+            indexingQueue: account.collection('indexing_queue'),
+        }
+        return {
+            ...collections,
+            movie: (id: string) => collections.moviesCollection.doc(id),
+            series: (id: string) => collections.seriesCollection.doc(id),
+            season: (id: string) => collections.seasonsCollection.doc(id),
+            episode: (id: string) => collections.episodesCollection.doc(id),
+            uncategorizedFile: (putIoFileId: string) => collections.uncategorizedFiles.doc(putIoFileId),
+            indexingQueueEntry: (putIoFileId: string) => collections.indexingQueue.doc(putIoFileId),
+        };
+    },
+};
+
+export const db = {
+    ...dbCollections,
+    ...dbSingles,
+};
