584effc0ee00a13e4b7bb9918d937696eb71b2b1
Marcus Weiner <marcus.weiner@gmail.com>
2018-10-30 19:34:22 +0100

    :recycle: Move putio specific types to module

--- functions/src/functions/putio-webhook.ts
+++ functions/src/functions/putio-webhook.ts
@@ -6,75 +6,15 @@ import fetch from 'node-fetch';
 import { firestore } from '../util/firestore';
 import { collect } from '../util/iteration';
 import { File, IndexingQueueEntry } from '../util/types';
+import {
+    fileListUrl,
+    fileUrl,
+    PutIoFile,
+    PutIoFileType,
+    PutIoTransfer,
+    TransferStatus,
+} from '../util/putio';
 
-type FakeBool = 'True' | 'False';
-
-enum PutIoFileType {
-    Folder = 'FOLDER',
-    Text = 'TEXT',
-    Video = 'VIDEO',
-    Image = 'IMAGE',
-}
-
-interface PutIoFile {
-    content_type: string;
-    crc32: string | null;
-    created_at: string;
-    extension: string | null;
-    file_type: PutIoFileType;
-    first_accessed_at: string | null;
-    folder_type: 'REGULAR';
-    icon: string;
-    id: number;
-    is_hidden: boolean;
-    is_mp4_available: boolean;
-    is_shared: boolean;
-    name: string;
-    opensubtitles_hash: string | null;
-    parent_id: number;
-    screenshot: string | null;
-    size: number;
-}
-
-enum TransferStatus {
-    Completed = 'COMPLETED',
-    Downloading = 'DOWNLOADING',
-    InQueue = 'IN_QUEUE',
-}
-
-interface PutIoTransfer {
-    callback_url: string;
-    completion_percent: number;
-    created_at: string;
-    created_torrent: FakeBool;
-    current_ratio: number;
-    download_id: number;
-    downloaded: number;
-    down_speed: number;
-    extract: FakeBool;
-    file_id: number;
-    finished_at: string;
-    id: number;
-    is_private: FakeBool;
-    name: string;
-    percent_done: number;
-    peers_connected: number;
-    peers_getting_from_us: number;
-    peers_sending_to_us: number;
-    save_parent_id: number;
-    simulated: FakeBool;
-    size: number;
-    source: string;
-    status: TransferStatus;
-    status_message: string;
-    torrent_link: string;
-    type: string;
-    uploaded: number;
-    up_speed: number;
-}
-
-const fileUrl = (fileId: number) => `https://api.put.io/v2/files/${fileId}`;
-const fileListUrl = (parentId: number) => `https://api.put.io/v2/files/?parent_id=${parentId}`;
 
 async function* listDirectoryRecursive(dirId: number): AsyncIterable<PutIoFile> {
     const listResp = await fetch(fileListUrl(dirId));
---
+++ functions/src/util/putio.ts
@@ -0,0 +1,68 @@
+export type FakeBool = 'True' | 'False';
+
+export enum PutIoFileType {
+    Folder = 'FOLDER',
+    Text = 'TEXT',
+    Video = 'VIDEO',
+    Image = 'IMAGE',
+}
+
+export interface PutIoFile {
+    content_type: string;
+    crc32: string | null;
+    created_at: string;
+    extension: string | null;
+    file_type: PutIoFileType;
+    first_accessed_at: string | null;
+    folder_type: 'REGULAR';
+    icon: string;
+    id: number;
+    is_hidden: boolean;
+    is_mp4_available: boolean;
+    is_shared: boolean;
+    name: string;
+    opensubtitles_hash: string | null;
+    parent_id: number;
+    screenshot: string | null;
+    size: number;
+}
+
+export enum TransferStatus {
+    Completed = 'COMPLETED',
+    Downloading = 'DOWNLOADING',
+    InQueue = 'IN_QUEUE',
+}
+
+export interface PutIoTransfer {
+    callback_url: string;
+    completion_percent: number;
+    created_at: string;
+    created_torrent: FakeBool;
+    current_ratio: number;
+    download_id: number;
+    downloaded: number;
+    down_speed: number;
+    extract: FakeBool;
+    file_id: number;
+    finished_at: string;
+    id: number;
+    is_private: FakeBool;
+    name: string;
+    percent_done: number;
+    peers_connected: number;
+    peers_getting_from_us: number;
+    peers_sending_to_us: number;
+    save_parent_id: number;
+    simulated: FakeBool;
+    size: number;
+    source: string;
+    status: TransferStatus;
+    status_message: string;
+    torrent_link: string;
+    type: string;
+    uploaded: number;
+    up_speed: number;
+}
+
+export const fileUrl = (fileId: number) => `https://api.put.io/v2/files/${fileId}`;
+export const fileListUrl = (parentId: number) => `https://api.put.io/v2/files/?parent_id=${parentId}`;
