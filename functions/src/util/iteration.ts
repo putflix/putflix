f17ca6a922d9528ce92f9c628cefcc5537564636
Leo Bernard <admin@leolabs.org>
2018-11-02 16:07:45 +0100

    :fire: Remove unused iteration util

--- functions/src/util/iteration.ts
+++
@@ -1,7 +0,0 @@
-export async function collect<T>(it: AsyncIterable<T> | Iterable<T>): Promise<T[]> {
-    const elts = [];
-    for await (const elt of it) {
-        elts.push(elt);
-    }
-    return elts;
-}
