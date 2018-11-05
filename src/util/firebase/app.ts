645d62d06c86a9c4191d0446e9da3f3fae95ba5a
Marcus Weiner <marcus.weiner@gmail.com>
2018-11-05 21:38:49 +0100

    :art: Type firebase config explicitly

--- src/util/firebase/app.ts
+++ src/util/firebase/app.ts
@@ -2,5 +2,12 @@ import * as fb from 'firebase/app';
 
 import config from '../../firebase.config';
 
+export interface FirebaseInitOptions {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+}
+
 export const firebase = fb.initializeApp(config);
 export const firebaseNS = fb;
