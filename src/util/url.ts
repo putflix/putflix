6a23e15ec784fdce9f37b8dc88fe2400a199ec81
Leo Bernard <admin@leolabs.org>
2018-11-06 09:19:58 +0100

    :rotating_light: Fix line length of url lib

--- src/util/url.ts
+++ src/util/url.ts
@@ -1,1 +1,3 @@
-export const putIoAuthorizationUrl = `https://api.put.io/v2/oauth2/authenticate?client_id=${process.env.REACT_APP_PUTIO_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(location.origin)}`;
+export const putIoAuthorizationUrl = `https://api.put.io/v2/oauth2/authenticate` +
+  `?client_id=${process.env.REACT_APP_PUTIO_CLIENT_ID}` +
+  `&response_type=code&redirect_uri=${encodeURIComponent(location.origin)}`;
